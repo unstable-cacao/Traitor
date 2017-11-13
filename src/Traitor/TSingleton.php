7294958cc9f5deb12aa60ef2b9d87c15248e7b93
Marina Sherstuk <marinas@commpeak.com>
2017-11-13 09:20:54 +0200

    Created TSingletonLike and moved all the singleton logic from TSingleton

--- src/Traitor/TSingleton.php
+++ src/Traitor/TSingleton.php
@@ -5,28 +5,5 @@ namespace Traitor;
 trait TSingleton
 {
 	use TStaticClass;
-	
-	
-	/**
-	 * @var static
-	 */
-	private static $instance = null;
-	
-	
-	/**
-	 * @param static|TSingleton $instance
-	 */
-	protected static function initialize($instance) {}
-	
-	
-	public static function instance()
-	{
-		if (is_null(self::$instance))
-		{
-			self::$instance = new static();
-			self::initialize(self::$instance);
-		}
-		
-		return self::$instance;
-	}
+    use TSingletonLike;
 }
---
+++ src/Traitor/TSingletonLike.php
@@ -0,0 +1,29 @@
+<?php
+namespace Traitor;
+
+
+trait TSingletonLike
+{
+    /**
+     * @var static
+     */
+    private static $instance = null;
+    
+    
+    /**
+     * @param static|TSingletonLike $instance
+     */
+    protected static function initialize($instance) {}
+    
+    
+    public static function instance()
+    {
+        if (is_null(self::$instance))
+        {
+            self::$instance = new static();
+            self::initialize(self::$instance);
+        }
+        
+        return self::$instance;
+    }
+}
