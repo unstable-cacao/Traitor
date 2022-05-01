1901418f1b3ec070677b859d67d9a766e1b2d8f5
Marina Puchkov <marinashers@gmail.com>
2022-05-01 15:57:55 +0300

    Updated dependencies

--- Source/TStaticClass.php
+++ Source/TStaticClass.php
@@ -6,5 +6,10 @@ trait TStaticClass
 {
 	private function __construct() {}
 	private function __clone() {}
-	private function __wakeup() {}
+	
+	
+	public function __wakeup()
+	{
+		throw new \Error('This method is not allowed');
+	}
 }
